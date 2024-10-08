351b4aa16b9b5fa57b8a8cdecb04179de1bbf866
ShaimaKaraki <shaimakaraki@gmail.com>
2024-10-08 16:37:47 +0300

    add location url

--- lib/api_sabeq_ps_v1/sabeq_helpers.rb
+++ lib/api_sabeq_ps_v1/sabeq_helpers.rb
@@ -52,12 +52,14 @@ module ApiSabeqPsV1
     end
 
     def sabeq_api_v1_create_parcel(verification_token, name, phone1, phone2, content,
-                     payment_amount, area_id, address, delivery_notes, special_notes)
+                     payment_amount, area_id, address, location_url,
+                     delivery_notes, special_notes)
       auth_link = SABEQ_URL + "/api/v1/parcels"
       auth_json = { verification_token: verification_token,
                     name: name, phone1: phone1, phone2: phone2,
                     content: content, payment_amount: payment_amount,
-                    area_id: area_id, address: address,
+                    area_id: area_id,
+                    address: address, location_url: location_url,
                     delivery_notes: delivery_notes,
                     special_notes: special_notes }
       json_response = make_post_request(auth_link, auth_json)
@@ -66,13 +68,14 @@ module ApiSabeqPsV1
     end
 
     def sabeq_api_v1_1_create_parcel(verification_token, name, phone1, phone2, content,
-                     payment_amount, area_id, street_id, address, service_type,
-                     delivery_notes, special_notes)
+                     payment_amount, area_id, street_id, address, location_url,
+                     service_type, delivery_notes, special_notes)
       auth_link = SABEQ_URL + "/api/v1/parcels"
       auth_json = { verification_token: verification_token,
                     name: name, phone1: phone1, phone2: phone2,
                     content: content, payment_amount: payment_amount,
-                    area_id: area_id, street_id: street_id, address: address,
+                    area_id: area_id, street_id: street_id,
+                    address: address, location_url: location_url,
                     service_type: service_type,
                     delivery_notes: delivery_notes,
                     special_notes: special_notes }
--- lib/api_sabeq_ps_v1/version.rb
+++ lib/api_sabeq_ps_v1/version.rb
@@ -1,3 +1,3 @@
 module ApiSabeqPsV1
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
